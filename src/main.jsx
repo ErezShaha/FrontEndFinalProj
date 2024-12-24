ef0e3790aa714cf3171d532b9478c7bd7e6eed04
ErezShaha <erez.shaha123@gmail.com>
2024-12-24 14:45:39 +0200

    password vis

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 axios.defaults.baseURL = "http://localhost:8080";
 
@@ -13,7 +14,7 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path="/" element={<LoginPage/>} />
       <Route path="/home" element={<HomePage/>} />
-    </Routes>;
-  </BrowserRouter>;
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,70 +2,152 @@ import React from 'react';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import '../styles/LoginPage.css';
 
  
 
 
 const LoginPage = () => {
-  const [user, setuser] = useState({username:'', password:''});
   const navigate = useNavigate();
 
-  const login = async(e) => { e.preventDefault();
+  const [user, setuser] = useState({username:'', password:''});
+  const [userRegister, setuserRegister] = useState({username:'', password:''});
+  const [PassowrdVis, setPassVIs] = useState('password');
+  const [holdMouse, setholdMouse] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isCard, setisCard] = useState(true);
+
+  const toggleCard = (e) => {e.preventDefault()
+    setisCard(!isCard)
+  };  
+
+  const login = async(e) => { 
+    e.preventDefault();
+    setIsDisabled(true);
     try {
-      console.log('nigger')
       const res = await axios.post('/api/v1/users/login',user,{withCredentials: true});
       navigate('/home');
     } catch (error) {
       console.log(error)
     }
      };
+
+     const register = async(e) => {
+      try {
+        const res = await axios.post('/api/v1/users/signup',user,{withCredentials: true});
+      } catch (error) {
+        console.log(error)
+      }
+     }
+
      
     return (
-       <div className = 'signIn' >
-         <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>Nigger Website</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Bomboclat</Card.Subtitle>
-          <Card.Text>
-            you want to register or sign in?
-          </Card.Text>
-          
-          <InputGroup className="mb-3">
-        <Form.Control
-        value={user.userName}
-        onChange={(e) => setuser({...user, username: e.target.value})}
-          placeholder="Username"
-          aria-label="Username"
-          aria-describedby="signIn"
-        />
-      
+      <div>
+        {isCard ? (
+          //sign in
+          <div className = 'centerDiv' >
+             <Card style={{ width: '18rem' }}>
+         <Card.Body>
+           <h1 className='bigTitle'>Bomboclat</h1>
+           <Card.Subtitle className="mb-2 text-muted">Sign In</Card.Subtitle>
+           <Card.Title>
+             Username
+           </Card.Title>
+           
+           <InputGroup className="mb-3">
+         <Form.Control
+         value={user.userName}
+         onChange={(e) => setuser({...user, username: e.target.value})}
+           placeholder="Username"
+           aria-label="Username"
+           aria-describedby="signIn"
+         />
+         <br/>
+           </InputGroup>
 
-      
-        <Form.Control
-          value={user.Password}
-          onChange={(e) => setuser({...user, password: e.target.value})}
-          placeholder="Password"
-          aria-label="Password"
-          aria-describedby="signIn"
-        //   משפט טרנרי בטייפ בשביל לעשות שאפשר לראות
-          type='password'
-        />
-        <Button id='signIn' type='submit' onClick={login}  variant="info">Sign In</Button>
-      </InputGroup>
-         {/* is href ok? */}
+           <Card.Title>
+             Password
+           </Card.Title>
+
+           <InputGroup className="mb-3">
+         <Form.Control
+           value={user.password}
+           onChange={(e) => setuser({...user, password: e.target.value})}
+           placeholder="Password"
+           aria-label="Password"
+           aria-describedby="signIn"  
+           type={PassowrdVis}
+         />
+         <Button onMouseDown={()=> setPassVIs('text')} onMouseUp={()=> setPassVIs('password')}>o</Button>
+       </InputGroup> 
+         
+         <Button id='signIn' type='submit' onClick={login}  variant="info" disabled={isDisabled}>Sign In</Button>
+         <br/><br/>
+         <Button type='submit' variant="primary" onClick={toggleCard}>Register</Button>
+         
         
-      <Button type='submit' variant="info">Register</Button>
+         </Card.Body>
+       </Card>
+        </div>
+
+        ):(
+          //register
+          <div className='centerDiv'>
+            <Card style={{ width: '18rem' }}>
+         <Card.Body>
+           <h1 className='bigTitle'>Bomboclat</h1>
+           <Card.Subtitle className="mb-2 text-muted">Register</Card.Subtitle>
+           
+           <Card.Title>
+             Please enter a username
+           </Card.Title>
+           
+           <InputGroup className="mb-3">
+         <Form.Control
+         value={userRegister.userName}
+         onChange={(e) => setuserRegister({...user, username: e.target.value})}
+           placeholder="Username"
+           aria-label="Username"
+           aria-describedby="signIn"
+         />
+         <br/>
+           </InputGroup>
+
+           <Card.Title>
+           Please enter a password
+           </Card.Title>
+
+           <InputGroup className="mb-3">
+         <Form.Control
+           value={user.Password}
+           onChange={(e) => setuser({...user, password: e.target.value})}
+           placeholder="Password"
+           aria-label="Password"
+           aria-describedby="signIn"  
+           type={PassowrdVis}
+         />
+         <Button onMouseDown={()=> setPassVIs('text')} onMouseUp={()=> setPassVIs('password')}>o</Button>
+       </InputGroup>
+
+         
+         <Button type='submit' variant="primary">Register</Button>
+         <br/><br/>
+         <Button type='submit' onClick={toggleCard}  variant="info">Sign In</Button>
+        
+        
+         </Card.Body>
+       </Card>
+          </div>
+        )}
+      </div>
        
-        </Card.Body>
-      </Card>
-       </div>
-      );
+      )
     }
 
 export default LoginPage
+
